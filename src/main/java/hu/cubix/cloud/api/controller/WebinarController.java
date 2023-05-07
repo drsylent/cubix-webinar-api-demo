1e2216c421bf5ec7555ba8e176b419186ca347d7
Dávid Csendes <sylence96@gmail.com>
2023-05-07 15:00:17 +0200

    API application

---
+++ src/main/java/hu/cubix/cloud/api/controller/WebinarController.java
@@ -0,0 +1,41 @@
+package hu.cubix.cloud.api.controller;
+
+import hu.cubix.cloud.api.dto.WebinarRequest;
+import hu.cubix.cloud.api.dto.WebinarResponse;
+import hu.cubix.cloud.service.WebinarService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/webinar")
+public class WebinarController {
+
+    private static final Logger log = LoggerFactory.getLogger(WebinarController.class);
+
+    private final WebinarService service;
+
+    public WebinarController(WebinarService service) {
+        this.service = service;
+    }
+
+    @PostMapping(value = "/message",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public WebinarResponse webinar(@RequestBody WebinarRequest request) {
+        log.info("Request arrived on /webinar endpoint with message: {}, leftNumber: {}, rightNumber: {}",
+                request.message(), request.leftNumber(), request.rightNumber());
+        int result = service.calculate(request.leftNumber(), request.rightNumber());
+        service.createNewMessage(request.message());
+        List<String> recentMessages = service.retrieveRecentMessages();
+        log.info("Sending back response on /webinar endpoint with data: number of recentMessages {}, result: {}",
+                recentMessages.size(), result);
+        return new WebinarResponse(recentMessages, result);
+    }
+}
---
+++ src/main/java/hu/cubix/cloud/service/WebinarService.java
@@ -0,0 +1,35 @@
+package hu.cubix.cloud.service;
+
+import hu.cubix.cloud.call.api.MessageApi;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class WebinarService {
+
+    private static final Logger log = LoggerFactory.getLogger(WebinarService.class);
+
+    private final MessageApi api;
+
+    public WebinarService(MessageApi api) {
+        this.api = api;
+    }
+
+    public int calculate(int leftNumber, int rightNumber) {
+        log.info("Calculating: {} + {}", leftNumber, rightNumber);
+        return leftNumber + rightNumber;
+    }
+
+    public void createNewMessage(String message) {
+        log.info("Creating new message on API: {}", message);
+        api.newMessageArrived(message);
+    }
+
+    public List<String> retrieveRecentMessages() {
+        log.info("Retrieving recent messages from API");
+        return api.getRecentMessages();
+    }
+}
